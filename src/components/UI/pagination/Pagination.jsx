df4ab39c063b78f9b2dc66ba74d58a480caec7c5
Дмитрий <dmitriy.shupikov@gmail.com>
2022-06-06 13:14:58 +0700

    deploy

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -5,7 +5,7 @@ const Pagination = ({totalPages, page, changePage}) => {
     const pagesArray = getPagesArray(totalPages);
     return (
         <div className="btn__wrapper pagination">
-            {pagesArray.map(p =>
+            {pagesArray.map( p =>
                 <button className={page === p ? 'btn__current' : "nav__btn"}
                     onClick={() => changePage(p)} key={p}>{p}</button>)}
         </div>
